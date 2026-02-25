2c0494e2e6a3dc068657fbba27ee8b6c4d1caa39
ksj000503 <rlatjdwi0053@gmail.com>
2026-02-25 09:02:20 +0900

    ww

--- CCC.cpp
+++ CCC.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 int main()
 {
+	int First = 0;
+	int Second = 0;
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	cin >> First ;
+	cin >> Second ;
+
+	cout << First + Second - First * Second / First % Second << endl;
 	
 	return 0;
 }
